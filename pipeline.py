63262a13fb1a0d9d64ccca3759fb72dada75bb80
MINSOO KANG <stengine2@gmail.com>
2020-06-20 03:20:46 +0900

    Now you can test out the cause feedback yourself!
    The feedback cause is exported in .pkl format.
    All you have to do is change loadFile to True on the last line of pipeline.py
    run(loadFile=True, causeFilename="((cm,-0.4)=0.84)causeSunWed",outputDivider=900)
    
    for changing settings for the accuracy tests, edit line 59
    
    outputDividor will divid the total number of tweets and output only a few of the tweets you are loading.

--- pipeline.py
+++ pipeline.py
@@ -4,14 +4,14 @@ import Cause_organizer as cause
 import check_classifier as check
 from pickle import dump,load
 
-def run(progress = True, verbose = False, loadFile=False,causeFilename="causeSunWedFri"):
+def run(progress = True, verbose = False, loadFile=False,causeFilename="causeSunWedFri",outputDivider=900):
     if progress:
         classifier.run(Covid = True, verbose = verbose)
         #dates = ["../2020-04-19 Coronavirus Tweets.csv","../2020-04-21 Coronavirus Tweets.csv","../2020-04-22 Coronavirus Tweets.csv"]#,"../2020-04-24 Coronavirus Tweets.csv" ]
         # April overall Sun/Wed
-        #dates = (["../2020-03-29 Coronavirus Tweets.csv","../2020-04-01 Coronavirus Tweets.csv","../2020-04-05 Coronavirus Tweets.csv","../2020-04-08 Coronavirus Tweets.csv"]
-        #        +["../2020-04-{} Coronavirus Tweets.csv".format(i) for i in range(12,31,7)]
-        #        +["../2020-04-{} Coronavirus Tweets.csv".format(i) for i in range(15,31,7)])
+        dates = (["../2020-03-29 Coronavirus Tweets.csv","../2020-04-01 Coronavirus Tweets.csv","../2020-04-05 Coronavirus Tweets.csv","../2020-04-08 Coronavirus Tweets.csv"]
+                +["../2020-04-{} Coronavirus Tweets.csv".format(i) for i in range(12,31,7)]
+                +["../2020-04-{} Coronavirus Tweets.csv".format(i) for i in range(15,31,7)])
 
 
         # April overall Mon/Thu
@@ -21,10 +21,10 @@ def run(progress = True, verbose = False, loadFile=False,causeFilename="causeSun
 
         
         # April overall Sun/Wed/Fri
-        dates = (["../2020-03-29 Coronavirus Tweets.csv","../2020-04-01 Coronavirus Tweets.csv","../2020-04-03 Coronavirus Tweets.csv","../2020-04-06 Coronavirus Tweets.csv","../2020-04-08 Coronavirus Tweets.csv"]
-                  +["../2020-04-{} Coronavirus Tweets.csv".format(i) for i in range(10,31,7)]
-                  +["../2020-04-{} Coronavirus Tweets.csv".format(i) for i in range(12,31,7)]
-                  +["../2020-04-{} Coronavirus Tweets.csv".format(i) for i in range(15,31,7)])
+        #dates = (["../2020-03-29 Coronavirus Tweets.csv","../2020-04-01 Coronavirus Tweets.csv","../2020-04-03 Coronavirus Tweets.csv","../2020-04-06 Coronavirus Tweets.csv","../2020-04-08 Coronavirus Tweets.csv"]
+        #          +["../2020-04-{} Coronavirus Tweets.csv".format(i) for i in range(10,31,7)]
+        #          +["../2020-04-{} Coronavirus Tweets.csv".format(i) for i in range(12,31,7)]
+        #          +["../2020-04-{} Coronavirus Tweets.csv".format(i) for i in range(15,31,7)])
 
         # April overall Mon/Thu/Sat
         #dates = (["../2020-03-30 Coronavirus Tweets.csv","../2020-04-02 Coronavirus Tweets.csv","../2020-04-04 Coronavirus Tweets.csv","../2020-04-06 Coronavirus Tweets.csv","../2020-04-09 Coronavirus Tweets.csv"]
@@ -56,7 +56,7 @@ def run(progress = True, verbose = False, loadFile=False,causeFilename="causeSun
         classifier.run(Covid = True, verbose = verbose, feed_back = [cm4,cm4,cm4,cm4],sf=0)
         """
         
-        classifier.run(Covid = True, verbose = verbose, feed_back = [_A,_F,_J,_S],sf=-0.2)
+        classifier.run(Covid = True, verbose = verbose, feed_back = [cmFJS, cmAJS, cmAFS, cmAFJ],sf=-0.4)
 
         if loadFile:
             dateChunks=[# weekly analysis
@@ -79,7 +79,7 @@ def run(progress = True, verbose = False, loadFile=False,causeFilename="causeSun
                     ]
 
             for d in dateChunks:
-                check.run(dates=d,verbose=False,loadFile4result=True)
+                check.run(dates=d,verbose=False,loadFile4result=True,outDeminish=outputDivider)
         
         
     else:
@@ -95,7 +95,7 @@ def run(progress = True, verbose = False, loadFile=False,causeFilename="causeSun
         #dates = (["../2020-04-{} Coronavirus Tweets.csv".format(i) for i in range(10,16)]+["../2020-04-0{} Coronavirus Tweets.csv".format(i) for i in range(1,10)])        
         check_classifier.run(verbose = verbose)
 if __name__ == '__main__':
-    run(loadFile=False, causeFilename="causeSunWedSat")
+    run(loadFile=True, causeFilename="((cm,-0.4)=0.84)causeSunWed",outputDivider=900)
     
 """To try different accuracy settings
 edit line 59(or if not sure, line with classiferi.run(...)
